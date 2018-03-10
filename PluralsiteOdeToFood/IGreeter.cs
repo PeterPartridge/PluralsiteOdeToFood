f88b139778e191d10a25fe4491003413341f75ee
Peter Partridge <petertoby@aol.com>
2018-03-10 17:41:45 +0000

    Learning .net core 2

---
+++ PluralsiteOdeToFood/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace PluralsiteOdeToFood
+{
+    public interface IGreeter
+    {
+        string MessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string MessageOfTheDay()
+        {
+            //return "Greetings Human";
+            return _configuration["Greeting"];
+        }
+    }
+}

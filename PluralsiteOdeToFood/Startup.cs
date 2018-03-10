5d8cfb1903bca3d5fb385f504ec31ef4413f8aef
Peter Partridge <petertoby@aol.com>
2018-03-10 18:23:24 +0000

    MIddleware
    MVC core 2 middle ware is very flexiable and easier to create error
    pages and custom strings etc for devlopment and go live applications.
    Logging is much easier for all apllications.

--- PluralsiteOdeToFood/Startup.cs
+++ PluralsiteOdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace PluralsiteOdeToFood
 {
@@ -17,22 +18,64 @@ namespace PluralsiteOdeToFood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greet)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greet, ILogger<Startup> logger)
         {
+
+
+            
+
             if (env.IsDevelopment())
+            {
+               
+                app.UseDeveloperExceptionPage();
+            }
+            else
             {
                 app.UseDeveloperExceptionPage();
             }
 
             //string Greet = configuration["Greeting"];
 
+            //app.Use(next => 
+            //{
+            //    return async context =>
+            //    {
+            //        logger.LogInformation("Request Incoming");
+
+            //        if (context.Request.Path.StartsWithSegments("/mym"))
+            //        {
+            //            await context.Response.WriteAsync("hit");
+            //        }
+            //        else
+            //        {
+            //            await next(context);
+            //            logger.LogInformation("Response outgoing");
+            //        }
+            //    };
+            //});
+
+            //app.UseWelcomePage(new WelcomePageOptions
+            //{
+            //    Path= "/wp"
+            //});
+
+            // app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+            
+            //app.UseFileServer();
+
             app.Run(async (context) =>
             {
+             //   throw new Exception("Error");
+
                 string Greet = greet.MessageOfTheDay();
-                await context.Response.WriteAsync(Greet);
+                await context.Response.WriteAsync($"{Greet} : {env.EnvironmentName}");
             });
         }
     }
